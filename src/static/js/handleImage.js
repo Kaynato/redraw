1e34fc9361b2ea105c75abd1266144d7524e48fc
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-10-25 08:47:03 -0400

    Relative window size

--- src/static/js/handleImage.js
+++ src/static/js/handleImage.js
@@ -1,26 +1,30 @@
-var imageLoader = document.getElementById('file-input');
+// Set height and width of canvas
+const canvas = document.getElementById('myCanvas');
+
+const windowHeight = parseInt(window.innerHeight) - 200;;
+const windowWidth = window.innerWidth;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+// Load input files
+const imageLoader = document.getElementById('file-input');
 imageLoader.addEventListener('change', handleImage, false);
 
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 // Upload image from local drive
-function handleImage(e){
-    var reader = new FileReader();
+async function handleImage(e){
+    const reader = new FileReader();
     reader.onload = function(event){
-        var img = new Image();
+        const img = new Image();
         img.onload = function() {
-            canvas.width = window.innerWidth;
-            canvas.height = 1000;
-            ctx.drawImage(img,0,0);
+            canvas.width = windowHeight;
+            canvas.height = windowHeight;
+            ctx.drawImage(img, parseInt(windowWidth) / 5, 0);
         }
         img.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);
 }
 
-// Set height and width of canvas
-var canvas = document.getElementById('myCanvas');
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeigh;
