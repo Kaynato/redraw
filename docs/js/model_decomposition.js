f1b8cbca1437665a406d2983583c7555e3396dbb
Zicheng Gao <kaynato@live.com>
2017-10-25 19:56:05 -0400

    Define interfaces and implement seekForward

---
+++ docs/js/model_decomposition.js
@@ -0,0 +1,31 @@
+/*
+model_decomposition.js
+
+Decomposition network. Loaded with TensorFire.
+*/
+
+DecomposeModel = {
+
+	/*
+		Load model from json dict files.
+	*/
+	loadModel() {
+		throw new Error("Not implemented!");
+	},
+
+	/*
+		Convert image into a tensor.
+	*/
+	imageToTensor(image) {
+		throw new Error("Not implemented!");
+	}
+
+	/*
+		Convert image tensor to descriptive strokes.
+	*/
+	imageToStrokes(imageTensor) {
+		throw new Error("Not implemented!");
+	}
+
+}
+
---
+++ docs/js/predict_vector.js
@@ -0,0 +1,24 @@
+/*
+predict_vector.js
+
+Generation network. Loaded with TensorFire.
+*/
+
+GenerateModel = {
+
+	/*
+		Load model from json dict files.
+	*/
+	loadModel() {
+		throw Error("Not implemented!")
+	},
+
+	/*
+		Should return a stroke-descriptive ndarray. 
+		Update once we get TensorFire working.
+	*/
+	nextStroke(mpState) {
+		throw Error("Not implemented!")
+	}
+
+}
