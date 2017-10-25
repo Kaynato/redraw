2e5febb230c790419fba583eaccc98cf13f2087a
riggly <riggly@users.noreply.github.com>
2017-10-25 03:16:35 -0400

    Add files via upload
    inital attempt to set up unit testing based off mocha and chai testing framework seems more viable than jsunittest

---
+++ src/handleImageTest.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var handleImage = require('../../../src/static/js');
+
+describe('handleImage', handleImage() {
+	it('should upload an image', handleImage() {
+		assert.equal(hand
+	});
+});
