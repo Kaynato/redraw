25ccf0c94e4fc9daa838783bcf07289a491dc8e2
Aditya Malik <axm862@case.edu>
2017-10-25 22:38:59 -0400

    initial commit for export

---
+++ docs/js/downloadImage.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById("canvas-holder");
+var ctx = canvas.getContext("2d");
+
+/* REGISTER DOWNLOAD HANDLER */
+/* Only convert the canvas to Data URL when the user clicks.
+   This saves RAM and CPU ressources in case this feature is not required. */
+function dlCanvas() {
+  var dt = canvas.toDataURL('image/png');
+  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+  /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+  dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+  this.href = dt;
+};
+document.getElementById("dl").addEventListener('click', dlCanvas, false);
