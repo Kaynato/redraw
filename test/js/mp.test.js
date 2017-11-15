466e2bfc4b9bc1ba19c70306e9b9910b633a114d
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-11-14 22:40:54 -0500

    Delete unnecessary files

--- test/js/mp.test.js
+++
@@ -1,5 +0,0 @@
-describe('MPState', function() {
-    it('should show current stroke is null', async function() {
-        assert.isNull(MPState.getCurrentStroke(), 'Current Stroke is null')
-    });
-});
