805eacb1a0b1fc2cc410d932231515f7c6e4d748
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-11-15 13:24:20 -0500

    Modify file conventions names

--- docs/js/download_image.js
+++ docs/js/download_image.js
@@ -1,10 +1,9 @@
-
 var canvas = document.getElementById("canvas-holder");
 var ctx = canvas.getContext("2d");
 
 /* REGISTER DOWNLOAD HANDLER */
 /* Only convert the canvas to Data URL when the user clicks.
-   This saves RAM and CPU ressources in case this feature is not required. */
+   This saves RAM and CPU resources in case this feature is not required. */
 function exportData() {
   var dt = canvas.toDataURL('image/png');
   /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
--- docs/js/upload_image.js
+++ docs/js/upload_image.js
@@ -1,6 +1,6 @@
 "use strict";
 /**
- * @module uploadImage.js
+ * @module upload_image.js
  *
  * Handles image upload.
  */
