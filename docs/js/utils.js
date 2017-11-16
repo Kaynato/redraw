dd604dc605b1fe8cbc2afa42d44adab0f2e61fe5
Zicheng Gao <kaynato@live.com>
2017-11-16 07:47:29 -0500

    Switch to jsfeat lib with hough transform

--- docs/js/utils.js
+++ docs/js/utils.js
@@ -1,15 +1,18 @@
-"use strict";
 /**
  * @module utils.js
  * Should be processed with browerify (npm)
+
+ * DO NOT PUT "USE STRICT" ON THE TOP OF THIS DOCUMENT!
+ * THAT BREAKS THE LIBRARY LOADING!
  */
 
 // For browserify to grab requirements.
-ndarray = require('ndarray')
-ndpack = require('ndarray-pack')
-ndunpack = require('ndarray-unpack')
-ndshow = require('ndarray-show')
-ndops = require('ndarray-ops')
-zeros = require('zeros')
+ndarray = require('ndarray');
+ndpack = require('ndarray-pack');
+ndunpack = require('ndarray-unpack');
+ndshow = require('ndarray-show');
+ndops = require('ndarray-ops');
+zeros = require('zeros');
 
-jsfeat = require('jsfeat')
+// Client-side jsfeat is newer.
+// jsfeat = require('jsfeat');
